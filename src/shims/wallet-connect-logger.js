db0498b4bdade749d9209ec1a4b3a7c72634be07
kentbabin <58686361+kentbabin@users.noreply.github.com>
2026-01-09 19:36:06 +0800

    removed vite plugins

--- src/shims/wallet-connect-logger.js
+++
@@ -1,7 +0,0 @@
-import pkg from '@walletconnect/logger';
-
-export const generateChildLogger = pkg.generateChildLogger;
-export const generatePlatformLogger = pkg.generatePlatformLogger;
-export const getDefaultLoggerOptions = pkg.getDefaultLoggerOptions;
-export const getLoggerContext = pkg.getLoggerContext;
-export default pkg;
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,28 +1,16 @@
-import path from 'path';
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { cjsInterop } from 'vite-plugin-cjs-interop';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
-    cjsInterop({
-      dependencies: ['@walletconnect/logger'],
-    }),
   ],
-  resolve: {
-    alias: {
-      '@walletconnect/logger': path.resolve(__dirname, 'shims/wallet-connect-logger.js'),
-    },
-  },
   ssr: {
-    noExternal: ['@walletconnect/logger', '@reown/appkit-wallet'],
-  },
-  optimizeDeps: {
-    include: ['@walletconnect/logger', '@reown/appkit-wallet'],
+    target: "node",
+    noExternal: true,
   },
 });
